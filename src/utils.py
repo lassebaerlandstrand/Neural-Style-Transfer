3c6eda471e4f1a6ac5558e5dde5faea59de7507a
Lasse Bærland Strand <lasse.strand@appendconsulting.no>
2025-01-01 17:08:22 +0000

    feat: Use 255 pixel range for better results

--- src/utils.py
+++ src/utils.py
@@ -6,36 +6,43 @@ import os
 
 IMAGE_SIZE = 512
 
-# VGG19 is trained on ImageNet dataset, so we need to normalize the images
-# From https://pytorch.org/tutorials/advanced/neural_style_tutorial.html
-cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406])
-cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225])
+# VGG19 is trained on ImageNet dataset. 
+# Therefore we normalize the images with the mean and standard deviation of the ImageNet dataset.
+IMAGENET_MEAN_255 = torch.tensor([123.675, 116.28, 103.53])
+IMAGENET_STD_NEUTRAL = torch.tensor([1.0, 1.0, 1.0])
 
+# Using [0, 255] for pixel values provided much better results than [0, 1]
 transform = transforms.Compose([
     transforms.Resize(IMAGE_SIZE),
     transforms.CenterCrop((IMAGE_SIZE, IMAGE_SIZE)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=cnn_normalization_mean, std=cnn_normalization_std)
+    transforms.Lambda(lambda x: x * 255),
+    transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL)
 ])
 
 def _denormalize(tensor: torch.Tensor) -> torch.Tensor:
-    mean = cnn_normalization_mean.to(tensor.device).view(1, 3, 1, 1)
-    std = cnn_normalization_std.to(tensor.device).view(1, 3, 1, 1)
+    """Revert the normalization process to restore pixel values."""
+    mean = IMAGENET_MEAN_255.to(tensor.device).view(1, 3, 1, 1)
+    std = IMAGENET_STD_NEUTRAL.to(tensor.device).view(1, 3, 1, 1)
     return tensor * std + mean
 
 def _get_relative_path(path: str) -> str:
+    """Get the relative path of a file from the project root."""
     return os.path.join(os.path.dirname(os.path.dirname(__file__)), path)
 
 def load_image_as_tensor(path: str, device: torch.device = torch.device("cpu")) -> torch.Tensor:
+    """Load an image and return it as a normalized tensor."""
     relative_path = _get_relative_path(path)
     image = Image.open(relative_path).convert("RGB")
-    image = transform(image).unsqueeze(0)
+    image = transform(image).unsqueeze(0)  # Add batch dimension
     return image.to(device)
 
-def save_image(tensor: torch.Tensor, path: str) -> None:
+def save_image(tensor: torch.Tensor, path: str) -> str:
+    """Save a tensor as an image file. Returns the path to the saved image."""
     relative_path = _get_relative_path(path)
     tensor = _denormalize(tensor)
-    tensor = tensor.squeeze(0).clamp(0, 1).detach().cpu()
-    tensor = transforms.ToPILImage()(tensor)
-    tensor.save(relative_path)
-    
+    tensor = tensor.squeeze(0).clamp(0, 255).detach().cpu()
+    tensor = tensor.permute(1, 2, 0).numpy().astype("uint8")
+    image = Image.fromarray(tensor)
+    image.save(relative_path)
+    return relative_path
